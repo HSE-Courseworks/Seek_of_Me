47928546dc0dfca31069d28aa8404852a904c0a3
kuragaa <kenegesova.k@gmail.com>
2024-08-20 16:21:04 +0300

    Add respawn mechanics

--- Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -17,6 +17,8 @@ namespace UnityStandardAssets._2D
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
 
+        float nextTimeToSearch = 0;
+
         // Use this for initialization
         private void Start()
         {
@@ -32,6 +34,7 @@ namespace UnityStandardAssets._2D
 
             if (target == null)
             {
+                FindPlayer();
                 return;
             }
 
@@ -57,6 +60,19 @@ namespace UnityStandardAssets._2D
             transform.position = newPos;
 
             m_LastTargetPosition = target.position;
+        }
+
+        void FindPlayer ()
+        {
+            if (nextTimeToSearch <= Time.time)
+            {
+                GameObject searchRes = GameObject.FindGameObjectWithTag("Player");
+                if (searchRes != null)
+                {
+                    target = searchRes.transform;
+                }
+                nextTimeToSearch = Time.time + 0.5f;
+            }
         }
     }
 }
