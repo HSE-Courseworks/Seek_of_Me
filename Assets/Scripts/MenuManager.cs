551d0fc317dade45f95fd0d94e8537565a5d7e5c
Sheron4ik <sheronov.d@mail.ru>
2024-08-31 16:35:38 +0300

    update MenuManager: add sounds

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,13 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
-	public void StartGame() {
+    [SerializeField]
+    string hoverOverSound = "ButtonHover";
+
+    [SerializeField]
+    string pressButtonSound = "ButtonPress";
+
+    AudioManager audioManager;
+
+    void Start() {
+        audioManager = AudioManager.instance;
+        if (audioManager == null) {
+            Debug.LogError("No AudioManager found!");
+        } 
+    }
+
+    public void StartGame() {
+        audioManager.PlaySound(pressButtonSound);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame() {
+        audioManager.PlaySound(pressButtonSound);
         Debug.Log("QUIT THE GAME!");
         Application.Quit();
+    }
+
+    public void OnMouseOver() {
+        audioManager.PlaySound(hoverOverSound);
     }
 
 }
